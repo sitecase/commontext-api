e3b7e17b3f60eeaf8490870e3dd32c255dadb7e3
avimehenwal <avi.mehanwal@gmail.com>
2022-09-17 21:22:57 +0200

    feat: use prisma service
    - [x] add REST path controllers
    - [x] swagger open api
    
    #3

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PostService, PrismaService],
 })
 export class AppModule {}
