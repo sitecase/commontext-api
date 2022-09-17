56de24b0d40a4ce9849f4fe73312d03d5e8bf603
avimehenwal <avi.mehanwal@gmail.com>
2022-09-18 01:11:51 +0200

    refactor(nest): remove unused modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserService } from './user/user.service';
-import { PostService } from './post/post.service';
 import { PrismaService } from './prisma/prisma.service';
+import { LanguageModule } from './language/language.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, UserService, PostService, PrismaService],
+  imports: [LanguageModule],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
