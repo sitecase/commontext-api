c2c8354885e51598773d49cb238a9357b768d71e
avimehenwal <avi.mehanwal@gmail.com>
2022-09-17 18:24:51 +0200

    feat: use prisma-provider as nest service
    #1

--- apps/nest-gql/src/app.module.ts
+++ apps/nest-gql/src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
+import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  providers: [AppService, CatsService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
