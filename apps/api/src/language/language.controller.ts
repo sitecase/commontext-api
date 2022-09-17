794d0b3e49cd5e6001c9ff2e27157a5198939a37
avimehenwal <avi.mehanwal@gmail.com>
2022-09-18 01:28:35 +0200

    refactor(nest): :art: remove dist from tracking

--- apps/api/src/language/language.controller.ts
+++ apps/api/src/language/language.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post } from '@nestjs/common';
 
 @Controller('language')
-export class LanguageController {}
+export class LanguageController {
+  @Get()
+  readAllLanguages(): Array<any> {
+    return [1, 2];
+  }
+
+  @Post()
+  @HttpCode(201)
+  createLanguage(): Array<any> {
+    return [1, 2];
+  }
+}
