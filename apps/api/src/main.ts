03e5789cdc2b4ce1d7f0b401a97c5116a19a956d
avimehenwal <avi.mehanwal@gmail.com>
2022-09-18 17:10:38 +0200

    docs: :art: move docs to proper place #4

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,10 +8,12 @@ async function bootstrap() {
   });
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('common-text API')
+    .setDescription(
+      'sitecase/commontext-api exposing translations tokens to clients',
+    )
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
